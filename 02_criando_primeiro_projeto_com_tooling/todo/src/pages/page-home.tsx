83587d2cc86708929c898b1b4bf700a6ef553d6d
Rafael Simionato <rafaasimi@hotmail.com>
2025-07-06 16:46:42 -0300

    feat: criando componente de resumo das tarefas

--- 02_criando_primeiro_projeto_com_tooling/todo/src/pages/page-home.tsx
+++ 02_criando_primeiro_projeto_com_tooling/todo/src/pages/page-home.tsx
@@ -1,5 +1,12 @@
 import { Container } from "../components/container";
+import { TasksSummary } from "../core-components/tasks-summary";
 
 export function PageHome() {
-  return <Container>Home</Container>;
+  return (
+    <Container as="article" className="space-y-3">
+      <header className="flex items-center justify-between gap-2">
+        <TasksSummary />
+      </header>
+    </Container>
+  );
 }
