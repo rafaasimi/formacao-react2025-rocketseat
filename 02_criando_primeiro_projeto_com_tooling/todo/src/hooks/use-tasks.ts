4b0583d6184e51c940dae2190549d5b7447f6caa
Rafael Simionato <rafaasimi@hotmail.com>
2025-07-07 12:00:16 -0300

    feat: atualizando badges de resumo das tarefas

--- 02_criando_primeiro_projeto_com_tooling/todo/src/hooks/use-tasks.ts
+++ 02_criando_primeiro_projeto_com_tooling/todo/src/hooks/use-tasks.ts
@@ -1,12 +1,12 @@
 import useLocalStorage from "use-local-storage";
-import { TASKS_KEY, type Task } from "../models/task";
+import { TASKS_KEY, TaskState, type Task } from "../models/task";
 
 export function useTasks() {
   const [tasks] = useLocalStorage<Task[]>(TASKS_KEY, []);
 
   return {
     tasks,
-    tasksCount: tasks.length,
+    createdTasksCount: tasks.filter((task) => task.state === TaskState.Created).length,
     concludedTasksCount: tasks.filter((task) => task.concluded).length,
   };
 }
