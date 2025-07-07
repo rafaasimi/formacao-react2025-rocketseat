b88b5b26a526f5bf61b1f5045ea68b9d6c559aab
Rafael Simionato <rafaasimi@hotmail.com>
2025-07-07 11:39:47 -0300

    feat: deletar tarefa

--- 02_criando_primeiro_projeto_com_tooling/todo/src/core-components/task-item.tsx
+++ 02_criando_primeiro_projeto_com_tooling/todo/src/core-components/task-item.tsx
@@ -23,13 +23,16 @@ export function TaskItem({ task }: TaskItemProps) {
   );
 
   const [taskTitle, setTaskTitle] = useState(task.title || "");
-  const { updateTask, updateTaskStatus } = useTask();
+  const { updateTask, updateTaskStatus, deleteTask } = useTask();
 
   function handleEditTask() {
     setIsEditing(true);
   }
 
   function handleExitEditTask() {
+    if (task.state === TaskState.Creating) {
+      deleteTask(task.id);
+    }
     setIsEditing(false);
   }
 
@@ -50,6 +53,10 @@ export function TaskItem({ task }: TaskItemProps) {
     updateTaskStatus(task.id, checked);
   }
 
+  function handleDeleteTask() {
+    deleteTask(task.id);
+  }
+
   return (
     <Card size="md">
       {!isEditing ? (
@@ -66,7 +73,11 @@ export function TaskItem({ task }: TaskItemProps) {
             {task?.title}
           </Text>
           <div className="flex gap-1">
-            <ButtonIcon icon={TrashIcon} variant="tertiary" />
+            <ButtonIcon
+              icon={TrashIcon}
+              variant="tertiary"
+              onClick={handleDeleteTask}
+            />
             <ButtonIcon
               icon={PencilIcon}
               variant="tertiary"
--- 02_criando_primeiro_projeto_com_tooling/todo/src/hooks/use-task.ts
+++ 02_criando_primeiro_projeto_com_tooling/todo/src/hooks/use-task.ts
@@ -33,9 +33,14 @@ export function useTask() {
     );
   }
 
+  function deleteTask(id: string) {
+    setTasks(tasks.filter((task) => task.id !== id));
+  }
+
   return {
     prepareTask,
     updateTask,
     updateTaskStatus,
+    deleteTask,
   };
 }
