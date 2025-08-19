e768c49685377f2c935ef04801d6f4eb8af61d7b
Rafael Simionato <rafaasimi@hotmail.com>
2025-08-19 09:02:39 -0300

    aula: componente dialog

--- 03_avancando_nos_fundamentos/gallery-plus/src/App.tsx
+++ 03_avancando_nos_fundamentos/gallery-plus/src/App.tsx
@@ -12,6 +12,9 @@ import { InputCheckbox } from './components/input-checkbox';
 import { InputSingleFile } from './components/input-single-file';
 import { useForm } from 'react-hook-form';
 import { ImageFilePreview } from './components/image-file-preview';
+import { Dialog, DialogBody, DialogContent, DialogFooter, DialogHeader, DialogTrigger } from './components/dialog';
+import Text from './components/text';
+import { DialogClose } from '@radix-ui/react-dialog';
 
 export default function App() {
   const form = useForm();
@@ -78,6 +81,37 @@ export default function App() {
           replaceBy={<ImageFilePreview src={fileSource} alt='Imagem' />}
           {...form.register('file')}
         />
+      </div>
+
+      <div>
+
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button>Abrir modal</Button>
+          </DialogTrigger>
+
+          <DialogContent>
+            <DialogHeader>Adicionar foto</DialogHeader>
+            <DialogBody>
+              <Text as='div' className='mb-4'>Teste conteúdo do dialog</Text>
+
+              <InputSingleFile
+                form={form}
+                allowedExtensions={['png', 'jpg', 'jpeg', 'webp']}
+                maxFileSizeInMB={50}
+                replaceBy={<ImageFilePreview src={fileSource} alt='Imagem' />}
+                {...form.register('file')}
+              />
+            </DialogBody>
+
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button variant='secondary'>Cancelar</Button>
+              </DialogClose>
+              <Button variant='primary'>Adicionar</Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
       </div>
     </div>
   );
