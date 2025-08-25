d2e634f0a52fa5197c0fd518c09f589acb4d2c7c
Rafael Simionato <rafaasimi@hotmail.com>
2025-08-25 10:45:46 -0300

    aula: componente header

--- 03_avancando_nos_fundamentos/gallery-plus/src/pages/layout-main.tsx
+++ 03_avancando_nos_fundamentos/gallery-plus/src/pages/layout-main.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router";
+import { MainHeader } from "../components/main-header";
 
 export function LayoutMain() {
   return (
     <>
+      <MainHeader className="mt-9"/>
       <Outlet />
     </>
   )
