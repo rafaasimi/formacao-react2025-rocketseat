82b37a983588330d866f2b90f201c6ebc9eabed3
Rafael Simionato <rafaasimi@hotmail.com>
2025-10-28 08:49:16 -0300

    aula: mock do filtro por álbuns

---
+++ 03_avancando_nos_fundamentos/gallery-plus/src/contexts/albums/components/albums-filter.tsx
@@ -0,0 +1,50 @@
+import Button from '../../../components/button';
+import Skeleton from '../../../components/skeleton';
+import Text from '../../../components/text';
+import type { Album } from '../models/album';
+import cx from 'classnames';
+
+interface AlbumsFilterProps extends React.ComponentProps<'div'> {
+  albums: Album[];
+  loading?: boolean;
+}
+
+export function AlbumsFilter({
+  albums,
+  loading,
+  className,
+  ...props
+}: AlbumsFilterProps) {
+  return (
+    <div
+      className={cx('flex items-center gap-3.5 overflow-x-auto', className)}
+      {...props}
+    >
+      <Text variant="heading-small">Álbuns</Text>
+      <div className="flex gap-3">
+        {!loading ? (
+          <>
+            <Button size="sm" variant="primary" className="cursor-pointer">
+              Todos
+            </Button>
+            {!loading &&
+              albums.map((album) => (
+                <Button
+                  key={album.id}
+                  size="sm"
+                  variant="ghost"
+                  className="cursor-pointer"
+                >
+                  {album.title}
+                </Button>
+              ))}
+          </>
+        ) : (
+          Array.from({ length: 5 }).map((_, index) => (
+            <Skeleton className="w-28 h-7" key={`album-loading-${index}`} />
+          ))
+        )}
+      </div>
+    </div>
+  );
+}
--- 03_avancando_nos_fundamentos/gallery-plus/src/pages/page-home.tsx
+++ 03_avancando_nos_fundamentos/gallery-plus/src/pages/page-home.tsx
@@ -1,9 +1,18 @@
 import Container from '../components/container';
+import { AlbumsFilter } from '../contexts/albums/components/albums-filter';
 import { PhotosList } from '../contexts/photos/components/photos-list';
 
 export function PageHome() {
   return (
     <Container>
+      <AlbumsFilter
+        albums={[
+          { id: '1', title: 'Viagens' },
+          { id: '2', title: 'Natureza' },
+          { id: '3', title: 'Países' },
+        ]}
+        className="mb-9"
+      />
       <PhotosList
         photos={[
           {
