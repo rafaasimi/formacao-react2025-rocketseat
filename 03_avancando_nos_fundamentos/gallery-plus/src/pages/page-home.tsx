ea9375db6726461cc0fbe41e5f3a6a1f06610302
Rafael Simionato <rafaasimi@hotmail.com>
2025-10-28 08:28:18 -0300

    aula: mock da lista de fotos

--- 03_avancando_nos_fundamentos/gallery-plus/src/pages/page-home.tsx
+++ 03_avancando_nos_fundamentos/gallery-plus/src/pages/page-home.tsx
@@ -1,55 +1,79 @@
-import Container from "../components/container";
-import { PhotoWidget } from "../contexts/photos/components/photo-widget";
-import type { Photo } from "../contexts/photos/models/photo";
+import Container from '../components/container';
+import { PhotosList } from '../contexts/photos/components/photos-list';
 
 export function PageHome() {
   return (
     <Container>
-      <div className="grid grid-cols-4 gap-9">
-        <PhotoWidget
-          photo={{
+      <PhotosList
+        photos={[
+          {
             id: '1',
             title: 'Foto de uma praia paradisíaca',
-            imageId: 'https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+            imageId:
+              'https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
             albums: [
               { id: '1', title: 'Viagens' },
               { id: '2', title: 'Natureza' },
               { id: '3', title: 'Países' },
-            ]
-          }}
-        />
-
-        <PhotoWidget
-          photo={{
+            ],
+          },
+          {
             id: '2',
             title: 'Foto de uma praia paradisíaca',
-            imageId: 'https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+            imageId:
+              'https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
             albums: [
               { id: '1', title: 'Viagens' },
               { id: '2', title: 'Natureza' },
               { id: '3', title: 'Países' },
-            ]
-          }}
-        />
-
-        <PhotoWidget
-          photo={{
+            ],
+          },
+          {
             id: '3',
             title: 'Foto de uma praia paradisíaca',
-            imageId: 'https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+            imageId:
+              'https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
             albums: [
               { id: '1', title: 'Viagens' },
               { id: '2', title: 'Natureza' },
               { id: '3', title: 'Países' },
-            ]
-          }}
-        />
-
-        <PhotoWidget
-          photo={{} as Photo}
-          loading
-        />
-      </div>
+            ],
+          },
+          {
+            id: '4',
+            title: 'Foto de uma praia paradisíaca',
+            imageId:
+              'https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+            albums: [
+              { id: '1', title: 'Viagens' },
+              { id: '2', title: 'Natureza' },
+              { id: '3', title: 'Países' },
+            ],
+          },
+          {
+            id: '5',
+            title: 'Foto de uma praia paradisíaca',
+            imageId:
+              'https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+            albums: [
+              { id: '1', title: 'Viagens' },
+              { id: '2', title: 'Natureza' },
+              { id: '3', title: 'Países' },
+            ],
+          },
+          {
+            id: '6',
+            title: 'Foto de uma praia paradisíaca',
+            imageId:
+              'https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+            albums: [
+              { id: '1', title: 'Viagens' },
+              { id: '2', title: 'Natureza' },
+              { id: '3', title: 'Países' },
+            ],
+          },
+        ]}
+      />
     </Container>
-  )
+  );
 }
