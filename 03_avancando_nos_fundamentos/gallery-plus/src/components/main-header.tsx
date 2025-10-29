7683756014a8449ac0af57f794b786806d3b5741
Rafael Simionato <rafaasimi@hotmail.com>
2025-10-29 08:42:59 -0300

    aula: mock do dialog de “adicionar foto”

--- 03_avancando_nos_fundamentos/gallery-plus/src/components/main-header.tsx
+++ 03_avancando_nos_fundamentos/gallery-plus/src/components/main-header.tsx
@@ -1,16 +1,21 @@
-import Container from "./container";
-import Logo from '../assets/images/galeria-plus-full-logo.svg?react'
-import { Link } from "react-router";
-import cx from 'classnames'
-import Button from "./button";
-import { PhotosSearch } from "./photos-search";
-import Divider from "./divider";
+import Container from './container';
+import Logo from '../assets/images/galeria-plus-full-logo.svg?react';
+import { Link } from 'react-router';
+import cx from 'classnames';
+import Button from './button';
+import { PhotosSearch } from './photos-search';
+import Divider from './divider';
+import { PhotoNewDialog } from '../contexts/photos/components/photo-new-dialog';
 
-interface MainHeaderProps extends React.ComponentProps<typeof Container> { }
+interface MainHeaderProps extends React.ComponentProps<typeof Container> {}
 
 export function MainHeader({ className, ...props }: MainHeaderProps) {
   return (
-    <Container as="header" className={cx('flex justify-between items-center gap-10', className)} {...props}>
+    <Container
+      as="header"
+      className={cx('flex justify-between items-center gap-10', className)}
+      {...props}
+    >
       <Link to="/">
         <Logo className="h-5" />
       </Link>
@@ -20,9 +25,12 @@ export function MainHeader({ className, ...props }: MainHeaderProps) {
       <Divider orientation="vertical" className="h-10" />
 
       <div className="flex items-center gap-3">
-        <Button variant="primary">Nova foto</Button>
+        <PhotoNewDialog
+          trigger={<Button variant="primary">Nova foto</Button>}
+        />
+
         <Button variant="secondary">Criar álbum</Button>
       </div>
     </Container>
-  )
+  );
 }
---
+++ 03_avancando_nos_fundamentos/gallery-plus/src/contexts/photos/components/photo-new-dialog.tsx
@@ -0,0 +1,94 @@
+import { useForm } from 'react-hook-form';
+import Alert from '../../../components/alert';
+import Button from '../../../components/button';
+import {
+  Dialog,
+  DialogBody,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTrigger,
+} from '../../../components/dialog';
+import { ImagePreview } from '../../../components/image-preview';
+import { InputSingleFile } from '../../../components/input-single-file';
+import { InputText } from '../../../components/input-text';
+import Skeleton from '../../../components/skeleton';
+import Text from '../../../components/text';
+import type { Album } from '../../albums/models/album';
+
+interface PhotoNewDialogProps {
+  trigger: React.ReactNode;
+}
+
+export function PhotoNewDialog({ trigger }: PhotoNewDialogProps) {
+  const form = useForm();
+
+  // TODO: Apenas para mock
+  const isLoadingAlbum = false;
+  const albums: Album[] = [
+    { id: '1', title: 'Viagens' },
+    { id: '2', title: 'Natureza' },
+    { id: '3', title: 'Países' },
+  ];
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>{trigger}</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>Adicionar foto</DialogHeader>
+
+        <DialogBody className="flex flex-col gap-5">
+          <InputText placeholder="Adicione um título" maxLength={255} />
+
+          <Alert>
+            Tamanho máximo: 50MB
+            <br />
+            Você pode selecionar arquivos em PNG, JPG ou JPEG
+          </Alert>
+
+          <InputSingleFile
+            form={form}
+            allowedExtensions={['png', 'jpg', 'jpeg']}
+            maxFileSizeInMB={50}
+            replaceBy={<ImagePreview className="w-full h-56" />}
+          />
+
+          <div className="space-y-3">
+            <Text variant="label-small">Selecionar álbuns</Text>
+            <div className="flex flex-wrap gap-3 mt-3">
+              {!isLoadingAlbum &&
+                albums.length > 0 &&
+                albums.map((album) => (
+                  <Button
+                    key={album.id}
+                    variant="ghost"
+                    size="sm"
+                    className="truncate"
+                  >
+                    {album.title}
+                  </Button>
+                ))}
+
+              {isLoadingAlbum &&
+                Array.from({ length: 5 }).map((_, index) => (
+                  <Skeleton
+                    key={`album-loading-${index}`}
+                    className="w-20 h-7"
+                  />
+                ))}
+            </div>
+          </div>
+        </DialogBody>
+
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button variant="secondary">Cancelar</Button>
+          </DialogClose>
+
+          <Button variant="primary">Adicionar</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
