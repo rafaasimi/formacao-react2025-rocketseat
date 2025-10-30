85b5c90994ab41599608f058efb028cc0b4641ff
Rafael Simionato <rafaasimi@hotmail.com>
2025-10-30 08:56:17 -0300

    aula: configurando react query

--- 03_avancando_nos_fundamentos/gallery-plus/src/App.tsx
+++ 03_avancando_nos_fundamentos/gallery-plus/src/App.tsx
@@ -1,20 +1,24 @@
-import { BrowserRouter, Route, Routes } from 'react-router'
+import { BrowserRouter, Route, Routes } from 'react-router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import PageComponents from './pages/page-components';
 import { LayoutMain } from './pages/layout-main';
 import { PageHome } from './pages/page-home';
 import { PagePhotoDetails } from './pages/page-photo-details';
 
-export default function App() {
+const queryClient = new QueryClient();
 
+export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<LayoutMain />}>
-          <Route index element={<PageHome />} />
-          <Route path='/fotos/:id' element={<PagePhotoDetails />} />
-          <Route path='/componentes' element={<PageComponents />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<LayoutMain />}>
+            <Route index element={<PageHome />} />
+            <Route path="/fotos/:id" element={<PagePhotoDetails />} />
+            <Route path="/componentes" element={<PageComponents />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
